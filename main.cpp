3134607b8a7fef2044c7ecd1f22b30eb0f1a69f9
Willy9821 <wiparraguirre@mail.csuchico.edu>
2017-10-04 14:57:09 -0700

    Steps for the program to follow

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "bitmap.h"
+ 
+using namespace std
+
+int main()
+{
+/* 
+  Program 2 Functional Decomposition
+ 1. Ask user to enter the file name of an image in BMP format.
+ 2. Make sure user has submitted correct format if not let them know and ask to submit again.
+ 3. Assign rows and columns for each pixel.
+ 4. Create a increment so that it goes to each colum of every row.
+ 5. Find the average value of the three colors: red, blue, green.
+ 6. Then assign the three colors to the average value.
+ 7. Change the rest of the  colors in the original pixel into a greyscale.
+ 8. Once every pixel has been changed save as a new file.
+ 9. Display the new image to the user.
+*/
+return 0;
+}
+
+
+
+
+
+
