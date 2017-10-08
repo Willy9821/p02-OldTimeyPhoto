98b305d13e622ca3a04c4f1b421057ec0935fd3f
Willy9821 <wiparraguirre@mail.csuchico.edu>
2017-10-08 14:49:31 -0700

    Progress on p02

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "bitmap.h"
- 
+#include <cstdlib>
+#include <string>
+
 using namespace std;
 
-int rows;
-int columns;
+int row, column, color;
+string fileName;
 
 int main()
 {
 /* 
   Program 2 Functional Decomposition
  1. Ask user to enter the file name of an image in BMP format.
- 2. Make sure user has submitted correct format if not let them know and ask to submit again.
+ 2. Make sure user has submitted correct format if not let them know.
  3. Assign rows and columns for each pixel.
  4. Create a increment so that it goes to each colum of every row.
  5. Find the average value of the three colors: red, blue, green.
@@ -21,22 +24,41 @@ int main()
  8. Once every pixel has been changed save as a new file.
  9. Display the new image to the user.
 */
-    Bitmap image;
-    vector< vector <Pixel> > bmp;
-    Pixel rgb;
+ cout<< "Hello, please enter the file name of an image(must be in Windows BMP format)."<<endl;
+ cin>>fileName;
 
-    image.open("machupicchu.bmp");
-    bmp = image.toPixelMatrix();
+ if(fileName[0] == 'B' || fileName[1] == 'M')
+ {
+  Bitmap image;
+  vector< vector <Pixel> > bmp;
+  Pixel rgb;
 
-    cout<<"Machu Picchu has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<< " pixels high."<<endl;
+  image.open("machupicchu.bmp");
+  bmp = image.toPixelMatrix();
 
-    rgb = bmp[0][0];
-    rgb.red = 0;
+  cout<<"Machu Picchu has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<< " pixels high."<<endl;
+   
+     for(int row = 0; row < bmp.size(); row ++)
+       {
+        rgb = bmp[0][0];
+        color = rgb.red + rgb.green + rgb.blue / 3;
+        rgb.red = rgb.green = rgb.blue;
+       }  
+          for(int column = 0; column < bmp[0].size(); column ++)
+            {
+             rgb = bmp[0][0];
+             color = rgb.red + rgb.green + rgb.blue / 3;
+             rgb.red = rgb.green = rgb.blue;
+            }  
 
-    bmp[0][0] = rgb;
-    image.fromPixelMatrix(bmp);
-    image.save("machupicchu.bmp");
-
+        bmp[0][0] = rgb;
+        image.fromPixelMatrix(bmp);
+        image.save("machupicchu.bmp");        
+ }
+ else
+  {
+   cout<<"The file must be a BMP format"<<endl;
+  } 
 
 return 0;
 }
