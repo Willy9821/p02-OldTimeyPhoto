aa08f3b10445877f398f0f8bcc45235050ae637d
Willy9821 <wiparraguirre@mail.csuchico.edu>
2017-10-11 12:59:29 -0700

    Update on p02

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
 /* 
   Program 2 Functional Decomposition
  1. Ask user to enter the file name of an image in BMP format.
- 2. Make sure user has submitted correct format if not let them know.
+ 2. Make sure user has submitted correct format if not let them know and make them try again.
  3. Assign rows and columns for each pixel.
  4. Create a increment so that it goes to each colum of every row.
  5. Find the average value of the three colors: red, blue, green.
